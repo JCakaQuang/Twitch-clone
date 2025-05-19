9f18d9de0c129078832845f62b994d4e0c20eaed
miin0 <phammin000@gmail.com>
2025-05-19 22:01:23 +0700

    update view for authentication

--- video-twitch/tailwind.config.js
+++ video-twitch/tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // hoặc 'media' nếu muốn tự động theo hệ thống
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      // Tùy chọn thêm các biến custom
+    },
   },
   plugins: [],
-};
+}
