556465f78d259a931df305aa07e7fad505ea2899
miin0 <phammin000@gmail.com>
2025-06-08 16:26:03 +0700

    upload code before install @next/codemod@15.4.0-canary.71

--- video-twitch/app/(dashboard)/u/[username]/(home)/page.tsx
+++ video-twitch/app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,6 +1,24 @@
-const CreatorPage = () => {
+import { getUserByUsername } from "@/lib/user-service";
+import { currentUser } from "@clerk/nextjs/server";
+
+interface CreatorPageProps {
+    params: {
+        username: string;
+    };
+}
+
+const CreatorPage = async ({ 
+    params 
+}: CreatorPageProps) => {
+    const externalUser = await currentUser();
+    const user = await getUserByUsername(params.username);
+
+    if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+        throw new Error("Unauthorized access or user not found");
+    }
+
     return (
-        <div>
+        <div className="h-full">
             Creator Page
         </div>
     );
